3b8f3de259a7ebb18e3dd4ecd4e623b252a753bd
kaage <kateageage@gmail.com>
2020-08-07 01:20:14 +0900

    Converted spaces to tabs.

--- algebraic/FastFourierTransform.hpp
+++ algebraic/FastFourierTransform.hpp
@@ -3,38 +3,38 @@
 #include "MyComplex.hpp"
 class FastFourierTransform {
 private:
-    static void dft(std::vector<MyComplex>& func, const int& inverse) {
-        int sz = func.size();
-        if (sz == 1)return;
-        std::vector<MyComplex> veca, vecb;
-        rep(i, sz / 2) {
-            veca.push_back(func[2 * i]);
-            vecb.push_back(func[2 * i + 1]);
-        }
-        dft(veca, inverse); dft(vecb, inverse);
-        MyComplex now = 1, zeta = std::polar(1.0, inverse * 2.0 * acos(-1) / sz);
-        rep(i, sz) {
-            func[i] = veca[i % (sz / 2)] + now * vecb[i % (sz / 2)];
-            now *= zeta;
-        }
-    }
+	static void dft(std::vector<MyComplex>& func, const int& inverse) {
+		int sz = func.size();
+		if (sz == 1)return;
+		std::vector<MyComplex> veca, vecb;
+		rep(i, sz / 2) {
+			veca.push_back(func[2 * i]);
+			vecb.push_back(func[2 * i + 1]);
+		}
+		dft(veca, inverse); dft(vecb, inverse);
+		MyComplex now = 1, zeta = std::polar(1.0, inverse * 2.0 * acos(-1) / sz);
+		rep(i, sz) {
+			func[i] = veca[i % (sz / 2)] + now * vecb[i % (sz / 2)];
+			now *= zeta;
+		}
+	}
 public:
-    template<typename T>
-    static std::vector<double> multiply(const std::vector<T>& f, const std::vector<T>& g) {
-        std::vector<MyComplex> nf, ng;
-        int sz = 1;
-        while (sz < f.size() + g.size())sz *= 2;
-        nf.resize(sz); ng.resize(sz);
-        rep(i, f.size()) {
-            nf[i] = f[i];
-            ng[i] = g[i];
-        }
-        dft(nf, 1);
-        dft(ng, 1);
-        rep(i, sz)nf[i] *= ng[i];
-        dft(nf, -1);
-        std::vector<double> res(sz);
-        rep(i, sz)res[i]=nf[i].real() / sz;
-        return res;
-    }
+	template<typename T>
+	static std::vector<double> multiply(const std::vector<T>& f, const std::vector<T>& g) {
+		std::vector<MyComplex> nf, ng;
+		int sz = 1;
+		while (sz < f.size() + g.size())sz *= 2;
+		nf.resize(sz); ng.resize(sz);
+		rep(i, f.size()) {
+			nf[i] = f[i];
+			ng[i] = g[i];
+		}
+		dft(nf, 1);
+		dft(ng, 1);
+		rep(i, sz)nf[i] *= ng[i];
+		dft(nf, -1);
+		std::vector<double> res(sz);
+		rep(i, sz)res[i]=nf[i].real() / sz;
+		return res;
+	}
 };
--- algebraic/LagrangeInterpolation.hpp
+++ algebraic/LagrangeInterpolation.hpp
@@ -2,16 +2,16 @@
 #include "../other/template.hpp"
 #include "ModInt.hpp"
 ModInt arithmetic_lagrange_interpolation(const ModInt& a,const ModInt& d,const std::vector<ModInt>& y,const ModInt& t){
-    const int n=y.size()-1;
-    ModInt res=0,ft=1;
-    rep(i,n+1)ft*=t-(a+d*i);
-    ModInt f=1;
-    REP(i,n)f*=-d*i;
-    res+=y[0]/f*ft/(t-a);
-    rep(i,n){
-        f*=i+1;
+	const int n=y.size()-1;
+	ModInt res=0,ft=1;
+	rep(i,n+1)ft*=t-(a+d*i);
+	ModInt f=1;
+	REP(i,n)f*=-d*i;
+	res+=y[0]/f*ft/(t-a);
+	rep(i,n){
+		f*=i+1;
 		f/=i-n;
-        res+=y[i+1]/f*ft/(t-(a+d*(i+1)));
-    }
-    return res;
+		res+=y[i+1]/f*ft/(t-(a+d*(i+1)));
+	}
+	return res;
 }
