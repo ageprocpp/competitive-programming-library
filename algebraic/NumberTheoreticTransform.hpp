9e81f0f4be31a1453f9d585e13c2dc7332816d29
kaage <kateageage@gmail.com>
2020-08-07 23:08:01 +0900

    Excluded recursion from convolution_mod.

--- algebraic/NumberTheoreticTransform.hpp
+++ algebraic/NumberTheoreticTransform.hpp
@@ -10,7 +10,7 @@
 const unsigned int ModInt::modulo=998244353;
 class NumberTheoreticTransform{
 private:
-	static void ntt(std::vector<ModInt>& poly) {
+	/*static void ntt(std::vector<ModInt>& poly) {
 		int sz = poly.size();
 		if (sz == 1)return;
 		std::vector<ModInt> veca(sz>>1),vecb(sz>>1);
@@ -26,6 +26,24 @@ private:
 			poly[i]=veca[i%(sz>>1)]+now*vecb[i%(sz>>1)];
 			now*=zeta;
 		}
+	}*/
+	static void ntt(std::vector<ModInt>& a){
+		int sz=a.size();
+		if(sz==1)return;
+		ModInt root;
+		if(inverse)root=mypow(ModInt(3),ModInt::modulo-1-(ModInt::modulo-1)/sz);
+		else root=mypow(ModInt(3),(ModInt::modulo-1)/sz);
+		std::vector<ModInt> b(sz),roots((sz>>1)+1,1);
+		rep(i,sz>>1)roots[i+1]=roots[i]*root;
+		for(int i=sz>>1,w=1;w<sz;i>>=1,w<<=1){
+			for(int j=0;j<i;j++){
+				for(int k=0;k<w;k++){
+					b[k+((w*j)<<1)]=a[k+w*j]+a[k+w*j+(sz>>1)];
+					b[k+((w*j)<<1)+w]=roots[w*j]*(a[k+w*j]-a[k+w*j+(sz>>1)]);
+				}
+			}
+			std::swap(a,b);
+		}
 	}
 public:
 	static bool inverse;
@@ -34,7 +52,7 @@ public:
 		if(f.size()<g.size())std::swap(f,g);
 		std::vector<ModInt> nf, ng;
 		int sz=1;
-		while (sz<f.size()+g.size())sz>>=1;
+		while (sz<f.size()+g.size())sz<<=1;
 		nf.resize(sz);ng.resize(sz);
 		rep(i,f.size()) {
 			nf[i]=f[i];
