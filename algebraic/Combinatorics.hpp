4cddb4eff929be1586cdf7c03045c496af4e61de
kaage <kateageage@gmail.com>
2020-11-14 20:52:06 +0900

    Fixed.

--- algebraic/Combinatorics.hpp
+++ algebraic/Combinatorics.hpp
@@ -6,19 +6,19 @@ template<typename T>
 class Combinatorics{
 protected:
 	std::vector<T> factorial;
-	void append(int n){
+	void append(int n)noexcept{
 		while(factorial.size()<=n){
 			factorial.emplace_back(factorial.back()*factorial.size());
 		}
 	}
 public:
-	Combinatorics():factorial(1,1){}
-	Combinatorics(int n):factorial(1,1){append(n);}
-	virtual T getComb(int a,int b){
+	Combinatorics()noexcept:factorial(1,1){}
+	Combinatorics(int n)noexcept:factorial(1,1){append(n);}
+	virtual T getComb(int a,int b)noexcept{
 		append(a);
 		return factorial[a]/factorial[a-b]/factorial[b];
 	}
-	virtual T getDcomb(int a,int b){
+	virtual T getDcomb(int a,int b)noexcept{
 		return getComb(a+b-1,b);
 	}
 };
@@ -28,7 +28,7 @@ class ModCombinatorics:Combinatorics<T>{
 		||std::is_same<T,DynamicModInt>::value);
 	using Combinatorics<T>::factorial;
 	std::vector<T> inv;
-	void append(int n){
+	void append(int n)noexcept{
 		int tmp=factorial.size();
 		if(n<tmp)return;
 		Combinatorics<T>::append(n);
@@ -37,16 +37,16 @@ class ModCombinatorics:Combinatorics<T>{
 		for(int i=n;i>tmp;i--)inv[i-1]=inv[i]*i;
 	}
 public:
-	ModCombinatorics():Combinatorics<T>(),inv(1,1){}
-	ModCombinatorics(int n):Combinatorics<T>(n),inv(1,1){append(n);}
-	T getComb(int a,int b)override{
+	ModCombinatorics()noexcept:Combinatorics<T>(),inv(1,1){}
+	ModCombinatorics(int n)noexcept:Combinatorics<T>(n),inv(1,1){append(n);}
+	T getComb(int a,int b)noexcept override{
 		append(a);
 		return factorial[a]*inv[a-b]*inv[b];
 	}
-	T getDcomb(int a,int b)override{
+	T getDcomb(int a,int b)noexcept override{
 		return getComb(a+b-1,b);
 	}
-	T perm(int a,int b){
+	T perm(int a,int b)noexcept{
 		append(a);
 		return factorial[a]*inv[a-b];
 	}
