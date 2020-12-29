165eee37ea7b6278211bf29d9650b3c812101d60
kaage <kateageage@gmail.com>
2020-12-29 16:15:52 +0900

    Fixed.

--- data-structure/SparseTable.hpp
+++ data-structure/SparseTable.hpp
@@ -10,57 +10,37 @@ class SparseTable {
 	class initializer {
 	  public:
 		void operator()(SparseTable<T, wi>& st, const std::vector<T>& vec) {
-			int maxlength = 0;
-			while ((1 << (maxlength + 1)) <= vec.size()) maxlength++;
-			st.table.resize(maxlength + 1, std::vector<T>(vec.size()));
-			st.logtable.resize(vec.size() + 1);
-			rep(i, maxlength + 1) {
-				rep(j, vec.size() - (1 << i) + 1) {
-					if (i) {
-						st.table[i][j] =
-							std::min(st.table[i - 1][j],
-									 st.table[i - 1][j + (1 << (i - 1))]);
-					} else {
-						st.table[i][j] = vec[j];
-					}
-				}
-			}
-			st.logtable[1] = 0;
-			for (int i = 2; i <= vec.size(); i++) {
-				st.logtable[i] = st.logtable[i >> 1] + 1;
-			}
+			rep(i, vec.size()) st.table[0][i] = vec[i];
 		}
 	};
 	template <class dummy_type>
 	class initializer<true, dummy_type> {
 	  public:
 		void operator()(SparseTable<T, true>& st, const std::vector<T>& vec) {
-			int maxlength = 0;
-			while ((1 << (maxlength + 1)) <= vec.size()) maxlength++;
-			st.table.resize(maxlength + 1, std::vector<T>(vec.size()));
-			st.logtable.resize(vec.size() + 1);
-			rep(i, maxlength + 1) {
-				rep(j, vec.size() - (1 << i) + 1) {
-					if (i) {
-						st.table[i][j] =
-							std::min(st.table[i - 1][j],
-									 st.table[i - 1][j + (1 << (i - 1))]);
-					} else {
-						st.table[i][j] = {vec[j], j};
-					}
-				}
-			}
-			st.logtable[1] = 0;
-			for (int i = 2; i <= vec.size(); i++) {
-				st.logtable[i] = st.logtable[i >> 1] + 1;
-			}
+			rep(i, vec.size()) st.table[0][i] = {vec[i], i};
 		}
 	};
 
   public:
 	SparseTable() {}
 	SparseTable(const std::vector<T>& vec) { init(vec); }
-	void init(const std::vector<T>& vec) { initializer<withindex>()(*this, vec); }
+	void init(const std::vector<T>& vec) {
+		int maxlength = 0;
+		while ((1 << (maxlength + 1)) <= vec.size()) maxlength++;
+		table.resize(maxlength + 1, std::vector<T>(vec.size()));
+		logtable.resize(vec.size() + 1);
+		initializer<withindex>()(*this, vec);
+		REP(i, maxlength) {
+			rep(j, vec.size() - (1 << i) + 1) {
+				table[i][j] =
+					std::min(table[i - 1][j], table[i - 1][j + (1 << (i - 1))]);
+			}
+		}
+		logtable[1] = 0;
+		for (int i = 2; i <= vec.size(); i++) {
+			logtable[i] = logtable[i >> 1] + 1;
+		}
+	}
 	template <class InputIter>
 	SparseTable(InputIter first, InputIter last) {
 		std::vector<T> vec;
